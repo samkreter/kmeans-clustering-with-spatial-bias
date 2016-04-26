bec518bbb5bd584e7759c2ade339ecf485621a5f
Brendan Marsh <brendanmarsh@mwc-047225.dhcp.missouri.edu>
2016-04-25 22:35:28 -0500

    Added Spatial and Spectral Distance Metrics

--- cubeFunc.py
+++ cubeFunc.py
@@ -11,19 +11,20 @@ def ConvertDataCube(cube):
 	numSamples = xsize * ysize
 	numFeatures = wavelengths + 2
 
-	features = np.ones((numSamples,numFeatures))
+	data = np.ones((numSamples,numFeatures))
 
 	for i in range(xsize):
 		for j in range(ysize):
 
-			features[i*ysize + j][0] = i
-			features[i*ysize + j][1] = j
+			data[i*ysize + j][0] = i
+			data[i*ysize + j][1] = j
 
 			for k in range(2,numFeatures):
-				features[i*ysize + j][k] = cube[i][j][k-2]
+				data[i*ysize + j][k] = cube[i][j][k-2]
 
-	return features
+	return data
 
+# Converts the groundtruth (key) into 1D form compatible with the above features converted from the data cube
 def ConvertGroundtruth(GT):
 	xsize,ysize = GT.shape
 
@@ -35,6 +36,7 @@ def ConvertGroundtruth(GT):
 
 	return labels
 
+# Computes the rand index of two sets of classes, also apparently found in scikit
 def RandIndex(classes,key): # Should be 1D arrays of equal length. The values in the array should be classes
 
 	# https://en.wikipedia.org/wiki/Rand_index
@@ -62,22 +64,41 @@ def RandIndex(classes,key): # Should be 1D arrays of equal length. The values in
 
 	return (a+b)/(a+b+c+d)
 
-def EuclidDistance(features,i,j):
-	return math.sqrt( (features[i][0]-features[j][0])^2 + (features[i][1]-features[j][1])^2 )
+# Computes the physical distance between the i'th and j'th data point
+def SpatialDistanceEuclidean(data,i,j):
+	return math.sqrt( (data[i][0]-data[j][0])^2 + (data[i][1]-data[j][1])^2 )
 
-def NormalizeSpectra(features):
+def SpatialDistanceL2(data,i,j):
+	return (data[i][0]-data[j][0])^2 + (data[i][1]-data[j][1])^2 
 
-	numSamples, numFeatures = features.shape
+# Area normalizes the spectra such that the spectra of each data points integrates to 1
+def NormalizeSpectra(data):
+
+	numSamples, numFeatures = data.shape
 
 	for i in range(numSamples):
 		area = 0
 		for j in range(2,numFeatures):
-			area += features[i][j]
+			area += data[i][j]
 
 		for j in range(2,numFeatures):
-			features[i][j] /= area
+			data[i][j] /= area
 
-	return features
+	return data
+
+# Uses the L2 norm to define a "distance" between two data points in terms of their spectral components
+def SpectralDistanceL2(data,i,j):
+
+	numSamples, numFeatures = data.shape
+
+	L2 = 0
+
+	for k in range(2,numFeatures):
+		L2 += (data[i][k]-data[j][k])^2
+
+	return L2
+
+
 
 
 
